16f5840224af1609af76338ad3a22cc30db78f9c
keyaru77 <whyudacok@gmail.com>
2024-07-31 08:34:17 +0700

    Create UserPage.jsx

---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserPage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">User Page</h1>
+      <p>This is the user page content.</p>
+    </div>
+  );
+};
+
+export default UserPage;
