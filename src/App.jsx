cadc458d6772afdade2a0e552a30ec42eeacdcb9
keyaru77 <whyudacok@gmail.com>
2024-07-31 08:34:48 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import KomikPage from './pages/KomikPage';
+import UserPage from './pages/UserPage';
 import NavBar from './components/NavBar';
+import NavBottom from './components/NavBottom';
+import './App.css';  // Import your CSS file here
 
 const App = () => {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/komik/:id" element={<KomikPage />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <NavBar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/komik/:id" element={<KomikPage />} />
+            <Route path="/user" element={<UserPage />} />
+          </Routes>
+        </div>
+        <NavBottom />
+      </div>
     </Router>
   );
 };
