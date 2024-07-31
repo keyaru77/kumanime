86a2b542a7959e4b1457a63862ef6ed25ba846f0
keyaru77 <whyudacok@gmail.com>
2024-07-31 07:24:56 +0700

    Delete src directory

--- src/components/elements/TextDetail/TextDetail.jsx
+++
@@ -1,8 +0,0 @@
-const TextDetail = (props) => {
-    const {title, content} = props;
-    return(
-        <p className="text-[14px] py-1"><span className="font-bold">{title}:</span> {content || "-"}</p>
-    )
-}
-
-export default TextDetail;
