6e3fb9dbe1a23b3469442fa612c7ee85664d4a77
keyaru77 <whyudacok@gmail.com>
2024-07-31 08:10:49 +0700

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -64,6 +64,7 @@ const HomePage = () => {
         ))}
       </div>
     </div>
+    )}
     <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
           {updates.map((update, index) => (
@@ -78,8 +79,7 @@ const HomePage = () => {
             </div>
           ))}
         </div>
-      )}
-  );
+    );
 };
 
 export default HomePage;
